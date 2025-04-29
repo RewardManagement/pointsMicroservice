ef7a1f2b34511da9d396ff3fdbf2b3c972e9bd24
unnimaya5801 <unnimayas5801@gmail.com>
2025-04-29 17:15:41 +0530

    Optimize Points Microservice

--- src/main/java/com/rewards/points_service/controller/EventController.java
+++ src/main/java/com/rewards/points_service/controller/EventController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/events")
+@RequestMapping("api/events")
 public class EventController {
 
     private final EventService eventService;
